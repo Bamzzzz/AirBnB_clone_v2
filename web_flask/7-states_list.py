2c9834a8c88bc04d16d5950638382081c8cdf7c2
oladeji21 <oladejiajayi05@gmail.com>
2024-05-19 10:41:22 -0700

    all task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+"""starts a Flask web application for AirBnB clone"""
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Closes the database again at the end of the request.
+    """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """Display a HTML page of the States
+    """
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
